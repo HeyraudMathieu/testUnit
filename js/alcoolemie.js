9afd881706eedbee814b8087a4dbc0895bf927a0
mathieu.heyraud <mathieu.heyraud@CP-UFA-W10-PG18.sio.lan>
2020-09-03 10:55:04 +0200

    first commit test unitaire

---
+++ js/alcoolemie.js
@@ -0,0 +1,71 @@
+/**
+ * Fonction qui retourne l'alcool pur ingéré en fonction du nombre de verre
+ * 
+ * @param {type} nbVerres
+ * @returns {Number}
+ */
+function getAlcoolPur(nbVerres) {
+    const uniteAlcool = 10;
+    return nbVerres * uniteAlcool;
+}
+
+/**
+ * Fonction qui retourne le coefficient de diffusion en fonction du sexe
+ * 
+ * @param {type} sexe
+ * @returns {Number}
+ */
+function getCoefDiffusion(sexe) {
+    const coefDiffuH = 0.7, coefDiffuF = 0.6;
+    if(sexe === 'homme') {
+        return coefDiffuH;
+    } else {
+        return coefDiffuF;
+    }
+}
+
+/**
+ * Fonction qui retourne l'alcoolemie en fonction du sexe, du poids et du nombre de verres ingérés
+ * 
+ * @param {type} sexe
+ * @param {type} poids
+ * @param {type} nbVerres
+ * @returns {Number}
+ */
+function getAlcoolemie(sexe, poids, nbVerres) {
+    if(poids > 0) {
+        return (getAlcoolPur(nbVerres) / (poids * getCoefDiffusion(sexe))).toFixed(2);
+    } else {
+        return 0;
+    }
+}
+
+/**
+ * Fonction qui retourne l'amende encourue en fonction de l'alcoolémie
+ * 
+ * @param {type} alcoolemie
+ * @returns {String}
+ */
+function getAmende(alcoolemie) {
+    const seuil = 0.8;
+    if(alcoolemie < seuil) {
+        return 'Minorée : 90€ / Forfaitaire : 135€ / Majorée : 375€';
+    } else {
+        return '4500€';
+    }
+}
+
+/**
+ * Fonction qui retourne la sanction encourue en fonction de l'alcoolémie
+ * 
+ * @param {type} alcoolemie
+ * @returns {undefined}
+ */
+function getSanction(alcoolemie) {
+    const seuil = 0.8;
+    if(alcoolemie < seuil) {
+        return '6 points + suspension 3 ans'
+    } else {
+        return '6 points + 2 ans de prison + suspension 3 ans + stage de sensibilisation'
+    }
+}
